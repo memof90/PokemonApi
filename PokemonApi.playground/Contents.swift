527c7363fc6be4689598c078e3e702fcbf970295
memo figueredo <memof80@gmail.com>
2021-10-02 14:06:08 -0500

    added new decode form

--- PokemonApi.playground/Contents.swift
+++ PokemonApi.playground/Contents.swift
@@ -15,9 +15,22 @@ struct Result: Decodable {
     let url: String
 }
 
-struct SearchResultsTwo: Decodable {
-    let abilities: [ResultHabilities]
-    let weight: Int
+struct SearchResultsTwo: Decodable{
+    let abilities: [ResultHabilities]?
+    let types: [ResultsTypes]?
+    
+    enum CodingKeys: String, CodingKey {
+        case abilities
+        case ability
+        case types
+    }
+    
+    init(from decoder: Decoder) throws  {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.abilities = try container.decode([ResultHabilities]?.self, forKey: .abilities)
+        self.types = try container.decode([ResultsTypes].self, forKey: .types)
+       
+    }
 }
 
 struct ResultHabilities: Decodable {
@@ -30,6 +43,16 @@ struct abilityes: Decodable {
     let name: String
     let url: String
 
+}
+
+struct ResultsTypes: Decodable {
+    let slot: Int
+    let type: type
+}
+
+struct type : Decodable {
+    let name: String
+    let url: String
 }
 
 
@@ -86,7 +109,8 @@ fileprivate func secondCallApi(url: URL) {
 //            debugPrint(searchResultsTwo)
 
 //            print(searchResultsTwo.weight)
-            searchResultsTwo.abilities.forEach({print(" ability: \($0.ability.name), hidden: \($0.is_hidden), slot: \($0.slot)")})
+            searchResultsTwo.abilities?.forEach({print(" ability: \($0.ability.name), hidden: \($0.is_hidden), slot: \($0.slot)")})
+            searchResultsTwo.types?.forEach({print($0.slot)})
 
         } catch let jsonErrTwo {
             print("Failed Decode Json", jsonErrTwo)
@@ -94,4 +118,5 @@ fileprivate func secondCallApi(url: URL) {
     }.resume()
 }
 fetchPokemon()
+
 
